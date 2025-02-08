7435b3ef9e04ad24c5284b122c90822aacf8d289
Christian Bromley <christian.bromley@cruk.manchester.ac.uk>
2025-02-08 14:44:54 +0000

    feat: add thesis rag script

---
+++ website/thesis/thesis_rag.py
@@ -0,0 +1,6 @@
+"""Create a RAG app for my thesis."""
+# %%
+import pandas as pd
+
+# %%
+THESIS_PATH = "/Users/cbromley/Documents/Colab/bert_transformer/Christian_Bromley_Final_Thesis_20211221_cover.pdf"
